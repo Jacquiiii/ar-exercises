5cacaf48617c036ddaa271287587f398027d2fd8
Jacquiiii <jacqui.koroll@telus.com>
2023-01-23 19:20:09 -0800

    add exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jacqui", last_name: "Koroll", hourly_rate: 100)
+@store1.employees.create(first_name: "Callie", last_name: "Time", hourly_rate: 55)
+@store2.employees.create(first_name: "Jeremy", last_name: "Ruffalo", hourly_rate: 101)
+@store2.employees.create(first_name: "Maria", last_name: "Smith", hourly_rate: 55)
+@store2.employees.create(first_name: "Icabob", last_name: "Crane", hourly_rate: 25)
