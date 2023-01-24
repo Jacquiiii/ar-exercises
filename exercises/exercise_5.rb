8bc7758623eab21581fa8ff5c62164940c34b20b
Jacquiiii <jacqui.koroll@telus.com>
2023-01-23 19:08:04 -0800

    add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Total revenue across all stores is $#{Store.sum(:annual_revenue).to_i}"
+puts "Total average revenue of all stores is $#{Store.average(:annual_revenue).to_i}"
+puts "Number of stores generating $1M or more in annual sales: #{Store.where('annual_revenue > ?', 1000000).count(:all)}"
